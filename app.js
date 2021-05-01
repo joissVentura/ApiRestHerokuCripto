9db241409d1b96458a6c8468f540de419206c807
Joiss Ventura Rivera <2389790@gmail.com>
2021-05-01 10:26:05 -0500

    Disabling CORS

--- app.js
+++ app.js
@@ -7,7 +7,13 @@ const PORT = process.env.PORT || 3050;
 
 const app = express();
 app.use(bodyParser.json());
-
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+});
 // Credenciales mysql
 /* const connection = mysql.createConnection({
   host: 'us-cdbr-east-03.cleardb.com',
--- dist/app.dev.js
+++ dist/app.dev.js
@@ -8,7 +8,14 @@ var bodyParser = require('body-parser');
 
 var PORT = process.env.PORT || 3050;
 var app = express();
-app.use(bodyParser.json()); // Credenciales mysql
+app.use(bodyParser.json());
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Authorization, X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Allow-Request-Method');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+  res.header('Allow', 'GET, POST, OPTIONS, PUT, DELETE');
+  next();
+}); // Credenciales mysql
 
 /* const connection = mysql.createConnection({
   host: 'us-cdbr-east-03.cleardb.com',
